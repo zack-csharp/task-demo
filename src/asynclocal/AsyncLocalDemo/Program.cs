79c0e56d4597f87399e7ce804104eec97677cfe8
zack <wuzhiliang@cxist.com>
2021-10-12 10:32:45 +0800

    feat(asynclocal):distinguishing between threadstatic attribute and asynclocal

---
+++ src/asynclocal/AsyncLocalDemo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace AsyncLocalDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ src/asynclocal/ThreadStaticAndAsyncLocal/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadStaticAndAsyncLocal
+{
+    class Program
+    {
+        [ThreadStatic]
+        static string _threadStaticValue = "_threadStaticValue";
+
+        static AsyncLocal<string> _asyncLocalValue = new AsyncLocal<string>();
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 2; i++)
+            {
+                int loopValue = i;
+                Task.Run(async () =>
+                {
+                    _threadStaticValue = $"zack{loopValue}";
+
+                    _asyncLocalValue.Value = $"zack{loopValue}";
+
+                    Console.WriteLine($"LoopValue:{loopValue},ThreadId:{Thread.CurrentThread.ManagedThreadId},ThreadStaticValue:{_threadStaticValue}");
+
+                    Console.WriteLine($"LoopValue:{loopValue},ThreadId:{Thread.CurrentThread.ManagedThreadId},AsyncLocalValue:{_asyncLocalValue.Value}");
+
+                    await Task.Yield();
+
+                    await Task.Run(() =>
+                    {
+                        Console.WriteLine($"LoopValue:{loopValue},Other Task ThreadId:{Thread.CurrentThread.ManagedThreadId},ThreadStaticValue:{_threadStaticValue}");
+
+                        Console.WriteLine($"LoopValue:{loopValue},Other Task ThreadId:{Thread.CurrentThread.ManagedThreadId},AsyncLocalValue:{_asyncLocalValue.Value}");
+                    });
+                });
+            }
+
+            Task.Delay(TimeSpan.FromSeconds(1)).Wait();
+        }
+    }
+}
